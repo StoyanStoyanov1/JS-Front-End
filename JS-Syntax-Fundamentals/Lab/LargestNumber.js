9bf5e9575b287b16ba8bf33ed8c7f5c542fda778
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-09-27 09:32:33 +0200

    d

--- JS-Syntax-Fundamentals/Lab/LargestNumber.js
+++
@@ -1,6 +0,0 @@
-function LargestNumber(...numbers){
-    let theLargestNumber = numbers.sort((a, b) => b - a)[0]
-    console.log(`The largest number is ${theLargestNumber}.`)
-}
-
-LargestNumber(5, -3, 16)
