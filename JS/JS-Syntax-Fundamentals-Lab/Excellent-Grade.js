b51b43cff47fad21725031155d532847852912f7
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-10-13 09:56:29 +0200

    Create Excellent-Grade.js

---
+++ JS/JS-Syntax-Fundamentals-Lab/Excellent-Grade.js
@@ -0,0 +1,11 @@
+function excellentGrade(number){
+    if (number >= 5.50){
+        console.log('Excellent')
+    }
+    else {
+        console.log(`Not excellent`)
+    }
+}
+
+excellentGrade(5.50)
+excellentGrade(4.35)
