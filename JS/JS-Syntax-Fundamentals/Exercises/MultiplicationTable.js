f1c7ad31cac2102365e081199f82586e1fe2aabf
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-10-15 09:22:54 +0200

    Create MultiplicationTable.js

---
+++ JS/JS-Syntax-Fundamentals/Exercises/MultiplicationTable.js
@@ -0,0 +1,8 @@
+function multiplicationTable(number) {
+    for (let times = 1; times <= 10; times++) {
+        console.log(`${number} X ${times} = ${number * times}`)
+    }
+}
+
+multiplicationTable(5)
+multiplicationTable(2)
