d065061592418242f7a46e45f2dd2b74606dc1b8
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-02-28 07:50:11 +0100

    Create MonthPrinter.js

---
+++ JS-SyntaxFundamentals/Lab/MonthPrinter.js
@@ -0,0 +1,50 @@
+function MonthPrinter(month) {
+    let result;
+    switch (month) {
+        case 1:
+            result = "January";
+            break;
+        case 2:
+            result = "February";
+            break;
+        case 3:
+            result = "March";
+            break;
+        case 4:
+            result = "April";
+            break;
+        case 5:
+            result = "May";
+            break;
+        case 6:
+            result = "June";
+            break;
+        case 7:
+            result = "July";
+            break;
+        case 8:
+            result = "August";
+            break;
+        case 9:
+            result = "September";
+            break;
+        case 10:
+            result = "October";
+            break;
+        case 11:
+            result = "November";
+            break;
+        case 12:
+            result = "December";
+            break;
+        case month:
+            result = "Error!";
+            break;
+    }
+
+    console.log(result)
+
+}
+
+MonthPrinter(2)
+MonthPrinter(13)
